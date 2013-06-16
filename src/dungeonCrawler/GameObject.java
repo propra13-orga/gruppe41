897f11ee7f521d788e744ceab6583a24de5a7b24
MatthiasLittmann <matthias.littmann@uni-duesseldorf.de>
2013-06-16 13:59:01 +0200

    Added GameObjects

---
+++ src/dungeonCrawler/GameObject.java
@@ -0,0 +1,11 @@
+package dungeonCrawler;
+
+public abstract class GameObject {
+
+	public GameObject() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public abstract void performOn(GameElement element);
+
+}
