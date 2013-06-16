6b099ac883ddee4617d08754411333c92dfc316c
MatthiasLittmann <matthias.littmann@uni-duesseldorf.de>
2013-06-17 01:01:39 +0200

    Add Spell

--- src/dungeonCrawler/GameElements/Player.java
+++ src/dungeonCrawler/GameElements/Player.java
@@ -57,6 +57,7 @@ public class Player extends GameElement {
 	
 	public void increaseHealt(int Health) {
 		this.Health += Health;
+		if(this.Health > this.maxHealth) this.Health = this.maxHealth;
 	}
 	
 	public void reduceHealth(int Health, GameLogic logic) {
@@ -76,6 +77,14 @@ public class Player extends GameElement {
 				logic.teleportElement(this, logic.getCheckPoint());
 			}
 		}
+	}
+	
+	public boolean reduceMana(int mana, GameLogic logic) {
+		if (this.mana-mana >= 0){
+			this.mana = this.mana-mana;
+			return true;
+		}
+		return false;
 	}
 	
 	public int getHealt() {
